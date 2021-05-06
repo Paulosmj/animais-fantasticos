9f3078537b275154992484f3ef10013694a3f4f0
Paulosmj <psmeneguesso@gmail.com>
2021-05-06 17:16:13 -0300

    primeiro commit

---
+++ js/modules/outsideclick.js
@@ -0,0 +1,21 @@
+export default function outsideClick(element, events, callback) {
+  const html = document.documentElement;
+  const outside = 'data-outside';
+
+  function handleOutsideClick(event) {
+    if (!element.contains(event.target)) {
+      element.removeAttribute(outside);
+      events.forEach(() => {
+        html.removeEventListener('click', handleOutsideClick);
+      });
+      callback();
+    }
+  }
+
+  if (!element.hasAttribute(outside)) {
+    events.forEach((userEvent) => {
+      setTimeout(() => html.addEventListener(userEvent, handleOutsideClick));
+    });
+    element.setAttribute(outside, '');
+  }
+}
