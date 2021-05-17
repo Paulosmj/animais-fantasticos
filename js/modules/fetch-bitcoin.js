2de371f92c88b62f50387007f460a41136d804d6
Paulosmj <psmeneguesso@gmail.com>
2021-05-17 17:30:42 -0300

    fetch bitcoin refatorado

--- js/modules/fetch-bitcoin.js
+++ js/modules/fetch-bitcoin.js
@@ -1,10 +1,8 @@
-export default function initFetchBitcoin() {
-
+export default function fetchBitcoin(url, target) {
+  fetch(url)
+    .then((response) => response.json())
+    .then((bitcoin) => {
+      const btcPreco = document.querySelector(target);
+      btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
+    }).catch((erro) => console.log(Error(erro)));
 }
-// https://blockchain.info/ticker
-fetch('https://blockchain.info/ticker')
-  .then((response) => response.json())
-  .then((bitcoin) => {
-    const btcPreco = document.querySelector('.btc-preco');
-    btcPreco.innerText = (100 / bitcoin.BRL.sell).toFixed(4);
-  }).catch((erro) => console.log(Error(erro)));
